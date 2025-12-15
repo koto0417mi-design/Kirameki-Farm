032dab03846216400d6fcaf85a4b71c0cbf9d8d0
miyu-togano <koto0417.mi@gmail.com>
2025-12-15 14:35:14 +0900

    アバウトまで実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,10 @@
-// ハンバーガーメニュー本体
+// ハンバーガーメニュー//
 var hamburgerMenu = $('.header__menu');
 
-// ボタン → メニュー開閉
 $('.header__hamburger').on('click', function () {
     hamburgerMenu.toggleClass('header__menu--active');
 });
 
-// リサイズ時は必ず閉じる
 $(window).on('resize', function () {
     hamburgerMenu.removeClass('header__menu--active');
 });
