de865d044af5eabddcdd21f67f38c30c35365ba5
miyu-togano <koto0417.mi@gmail.com>
2025-12-22 15:40:46 +0900

    プライスまで

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,10 +1,26 @@
-// ハンバーガーメニュー//
-var hamburgerMenu = $('.header__menu');
+$(function () {
 
-$('.header__hamburger').on('click', function () {
+  // ハンバーガー
+  const hamburgerMenu = $('.header__menu');
+
+  $('.header__hamburger').on('click', function () {
     hamburgerMenu.toggleClass('header__menu--active');
-});
+  });
 
-$(window).on('resize', function () {
+  $(window).on('resize', function () {
     hamburgerMenu.removeClass('header__menu--active');
+  });
+
+  // ギャラリー Swiper
+  if (typeof Swiper !== 'undefined' && $('.gallery__slider').length) {
+    new Swiper('.gallery__slider', {
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: '.gallery__pagination',
+        clickable: true,
+      },
+    });
+  }
+
 });
