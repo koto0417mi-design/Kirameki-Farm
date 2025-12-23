c8073f8b27af89a4e4aee113c0b5cf3e2275da50
miyu-togano <koto0417.mi@gmail.com>
2025-12-23 21:04:46 +0900

    faqt途中

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,41 @@
 $(function () {
 
-  // ハンバーガー
-  const hamburgerMenu = $('.header__menu');
+    // ハンバーガー
+    const hamburgerMenu = $('.header__menu');
 
-  $('.header__hamburger').on('click', function () {
-    hamburgerMenu.toggleClass('header__menu--active');
-  });
-
-  $(window).on('resize', function () {
-    hamburgerMenu.removeClass('header__menu--active');
-  });
-
-  // ギャラリー Swiper
-  if (typeof Swiper !== 'undefined' && $('.gallery__slider').length) {
-    new Swiper('.gallery__slider', {
-      slidesPerView: 1,
-      loop: true,
-      pagination: {
-        el: '.gallery__pagination',
-        clickable: true,
-      },
+    $('.header__hamburger').on('click', function () {
+        hamburgerMenu.toggleClass('header__menu--active');
     });
-  }
 
+    $(window).on('resize', function () {
+        hamburgerMenu.removeClass('header__menu--active');
+    });
+
+    // スワイパー
+    if (typeof Swiper !== 'undefined' && $('.gallery__slider').length) {
+        new Swiper('.gallery__slider', {
+            slidesPerView: 1,
+            loop: true,
+            pagination: {
+                el: '.gallery__pagination',
+                clickable: true,
+            },
+        });
+    }
+
+});
+
+//  アコーディオン
+document.querySelectorAll('.faq__title').forEach(title => {
+    title.addEventListener('click', () => {
+        const content = title.nextElementSibling;
+        const icon = title.querySelector('.faq__icon');
+
+        const isOpen = content.style.display === 'block';
+
+        content.style.display = isOpen ? 'none' : 'block';
+        icon.src = isOpen
+            ? '/assets/image/icon_plus.png'
+            : '/assets/image/icon_minus.png';
+    });
 });
